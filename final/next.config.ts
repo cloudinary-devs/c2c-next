7c3ab6cfbf5bae787030dfe2db5bd30cb39bbbf0
jlooper-cloudinary <jen.looper@gmail.com>
2025-12-05 16:00:52 -0500

    config changes for path resolution on Netlify

--- final/next.config.ts
+++ final/next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '10mb',
     },
   },
+  outputFileTracingIncludes: {
+    '/**': ['./products/products.json'],
+  },
 };
 
 export default nextConfig;
